ad71fdfeda45a653ef96663a758a63c890261199
guangzan <guangzan1999@outlook.com>
2021-08-19 17:24:19 +0800

    auto deploy

--- convert.js
+++ convert.js
@@ -1,15 +1,5 @@
 import fs from 'fs'
 import { themes } from './dist/acnb-themes-data.es.js'
 
-fs.writeFileSync('themes.json', JSON.stringify(themes))
-
-fs.readdirSync('./dist').forEach(function (fileName) {
-    fs.unlinkSync('./dist/' + fileName);
-});
-
-fs.rmdir('./dist', (err) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-})
+fs.writeFileSync('dist/themes.json', JSON.stringify(themes))
+fs.unlinkSync('./dist/acnb-themes-data.es.js')
