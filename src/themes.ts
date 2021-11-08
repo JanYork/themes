bde3a58140a030662f63024ed8e83b3e3a37ad6c
guangzan <guangzan1999@outlook.com>
2021-11-08 21:09:37 +0800

    auto deploy

--- src/themes.ts
+++ src/themes.ts
@@ -23,7 +23,7 @@ export const themes: Array<Theme> = [
   },
   {
     name: 'geek',
-    url: 'https://cdn.jsdelivr.net/gh/awescnb/acnb-theme-geek@latest/dist/geek.iife.js',
+    url: 'https://acnb.gitee.io/acnb-theme-geek/geek.iife.js',
   },
   {
     name: ['acg', 'reacg'],
